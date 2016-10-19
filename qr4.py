5ecc15f654d24b30c0ac7c95656da54efeca86a8
LaurentH <hemie143@gmail.com>
2016-10-19 13:34:05 +0200

    row generation-counting

--- qr4.py
+++ qr4.py
@@ -35,15 +35,37 @@ grid_size = 25
 
 grid = [[0 for i in range(grid_size)] for j in range(grid_size)]
 
+WHITE = 0
+BLACK = 1
+FREE = 2
 
 def row_count_blocks(row):
     grouped_row = [(k, sum(1 for i in g)) for k, g in itertools.groupby(row)]
-    result = sum([1 for s in grouped_row  if s[0] == 2])
+    result = sum([1 for s in grouped_row if s[0] == 1])
+    #print(result)
     return result
 
-def generate_row(grid_ref, blacks_total, blocks_total):
-    grid_ref_blacks = grid_ref.count(2)
-    blacks to place = blacks_total - grid_ref_blacks
+def generate_row(grid_ref, row_num, blacks_total, blocks_total):
+    this_row = grid_ref[row_num]
+    row_blacks = this_row.count(BLACK)
+    row_whites = this_row.count(WHITE)
+    blacks_to_place = blacks_total - row_blacks
+    row_cells_free = grid_size - row_blacks - row_whites
+    row_boxes_black = row_count_blocks(this_row)
+    indices_free = [d for d, x in enumerate(this_row) if x == FREE]
+    print(indices_free)
+    combo = 0
+    good = 0
+    for indices in itertools.combinations(indices_free, blacks_to_place):
+        combo += 1
+        row_iter = list(this_row)
+        for x in indices:
+            row_iter[x] = BLACK
+        row_iter_boxes = row_count_blocks(row_iter)
+        if row_iter_boxes == blocks_total:
+            good += 1
+            #yield row_iter
+    print('combo: {}-{}'.format(combo, good))
     return
 
 
@@ -54,10 +76,12 @@ def check_col(col):
 while True:
     for row_num in range(grid_size):
         # Does not iterate through all combinations yet
+        print('row: {}'.format(row_num))
         grid[row_num] = generate_row(grid_static, row_num, rule_hor_black[row_num], rule_hor_blocks[row_num])
+
 
     if all(check_col(col_num) for col_num in range(grid_size)):
         print('Found')
         print(grid)
         break
-
+    break
